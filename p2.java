c36d23ff3cb2b3c8cd67a0356010a461cfbc5f06
mahe <barnwalmahe@gmail.com>
2026-01-09 00:45:32 +0530

    day2

---
+++ p2.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class p2_5709
+{
+	public static void main(String args[]) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter marks: ");
+		int marks_5709 = sc.nextInt();
+		if(marks_5709 >=90)
+		{
+			System.out.println("O");
+		}
+		else if(marks_5709 >=80)
+		{
+			System.out.println("E");
+		}
+		else if(marks_5709 >=70)
+		{
+			System.out.println("A");
+		}
+		else if(marks_5709 >=60)
+		{
+			System.out.println("B");
+		}
+		else if(marks_5709 >=50)
+		{
+			System.out.println("C");
+		}
+	
+	}	
+}

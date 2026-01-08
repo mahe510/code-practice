83236d54ebcbd604c3ca30ae0198a8e651e070f3
mahe <barnwalmahe@gmail.com>
2026-01-08 14:56:01 +0530

    day1

---
+++ p1.java
@@ -0,0 +1,10 @@
+class p1_5709
+{
+	public static void main(String args[])
+	{
+		System.out.println("Name: Mahe");
+		System.out.println("Roll: 2305709");
+		System.out.println("Branch: CSE");
+		System.out.println("Section: 27");
+	}
+}

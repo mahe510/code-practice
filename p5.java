3f615efc706273ad73613e767fbaccd1d449b2a1
mahe <barnwalmahe@gmail.com>
2026-01-09 19:04:34 +0530

    Day 4: Java practice

---
+++ p5.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class p5_5709
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter 1st name");
+		String first = sc.nextLine();
+		System.out.println("enter 2nd name");
+		String second = sc.nextLine();
+		System.out.println("reverse order is");
+		System.out.println(second +" "+ first);
+	}
+}

480997457f94e55832772f93e0da3071ebe00b2a
Mahe Barnwal <mahebarnwal@gmail.com>
2026-01-17 14:03:16 +0530

    l

--- p1.java
+++ p1.java
@@ -1,10 +1,14 @@
 class p1_5709
 {
-	public static void main(String args[])
+	public static void main(String arg[])
 	{
-		System.out.println("Name: Mahe");
-		System.out.println("Roll: 2305709");
-		System.out.println("Branch: CSE");
-		System.out.println("Section: 27");
+		int x = Integer.parseInt(arg[0]);
+		int y = Integer.parseInt(arg[1]);
+		int z = Integer.parseInt(arg[2]);
+		int l = (x>y)?((x>z)?x:z):((y>z)?y:z);
+		System.out.println("largest element is "+l);
 	}
 }
+	
+
+		
